03ca03e69bf99e19688dcefef2ac8936c4d02be8
Arttu <arttukayra@gmail.com>
2024-07-30 22:57:04 +0300

    init

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "./router";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ brr: "brr" });
+});
+
+app.use("/api", router);
+
+export default app;
